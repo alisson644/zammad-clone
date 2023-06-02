d276c17de76c0ebec35f40669ba6ca0639cb5484
Álisson Henrique <alissonhmb@gmail.com>
2023-06-02 16:31:37 -0300

    spec/factories/active_job_lock.rb copied

--- spec/factories/vendor/net/ldap/entry.rb
+++ spec/factories/vendor/net/ldap/entry.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
 FactoryBot.define do
-  
+
   # add custom attributes via:
   # mocked_entry         = build(:ldap_entry)
   # mocked_entry['attr'] = [value, another_value]
