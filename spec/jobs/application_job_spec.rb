d120638bd41c2bd5d1db7a6bfb4ccabe22281985
Álisson Henrique <alissonhmb@gmail.com>
2023-11-23 16:38:23 -0300

    spec/jobs/application_job_spec.rb copied

---
+++ spec/jobs/application_job_spec.rb
@@ -0,0 +1,20 @@
+# Copyright (C) 2012-2023 Zammad Foundation, htpps://zammad-foundation.org/
+
+require 'rails_helper'
+
+class FailingTestJob < ApplicationJob
+  retry_on(StandardError, attempts: 5)
+
+  def perform
+    Rails.logger.debug 'Failing'
+    raise 'some error...'
+  end
+end
+
+RSpec.describe ApplicationJob do
+  
+  it 'syncs ActiveJob#executions to delayed::Job#attempts' do
+    FailingTestJob.perform_later
+    expect { Delayed::Worker.new.workk_off }.to change { Delayed::Job.last.attempts }
+  end
+end
