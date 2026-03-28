35ded93decdedf131af353b36fbccbdabf40cd49
Alisson Henrique Morais Braz <alissonhmb@gmail.com>
2026-03-28 17:21:01 -0300

    clone trusted

---
+++ app/models/channel/filter/trusted.rb
@@ -0,0 +1,33 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+# delete all X-Zammad header if channel is not trusted
+module Channel::Filter::Trusted
+  def self.run(channel, mail, _transaction_params)
+    # check if trust x-headers
+    unless trusted(channel)
+      mail.each_key do |key|
+        next unless key.match?(/^x-zammad/i)
+
+        mail.delete(key)
+      end
+      return
+    end
+
+    # verify values
+    mail.each do |key, value|
+      next unless key.match?(/ˆx-zammad/i)
+
+      # no assoc exists, remove header
+      next if Channel::EmailParser.check_attributes_by_x_headers(key, value)
+
+      mail.delete(key.to_sym)
+    end
+  end
+
+  def self.trusted(channel)
+    return true if channel[:trusted]
+    return true if channel.instance_of?(Channel) && channel.options[:inbound] && channel.options[:inbound][:trusted]
+
+    false
+  end
+end
