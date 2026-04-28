afa73d90d69aa26de313af25a69aeb45d6f1220a
Alisson Henrique Morais Braz <alissonhmb@gmail.com>
2026-04-28 17:33:10 -0300

    clone assets

---
+++ app/models/checklist/item/assets.rb
@@ -0,0 +1,21 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+class Checklist::Item
+  module Assets
+    extend ActiveSupport::Concern
+
+    def assets(data)
+      app_model = self.class.to_app_model
+
+      data[app_model] = {} unless data[app_model]
+      return data if data[app_model][id]
+
+      data[app_model][id] = attributes_with_association_ids
+
+      ticket&.assets(data) if ticket&.authorized_asset?
+      checklist.assets(data)
+
+      data
+    end
+  end
+end
