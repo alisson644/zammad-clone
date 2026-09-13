e90e19a806b20400fab2b8311547d3d815319121
Alisson Henrique Morais Braz <alissonhmb@gmail.com>
2026-09-13 15:17:11 -0300

    cloned data_privacy_deletion_task

---
+++ app/models/concerns/perform_changes/action/data_privacy_deletion_task.rb
@@ -0,0 +1,16 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+class PerformChanges::Action::DataPrivacyDeletionTask < PerformChanges::Action
+  def self.phase
+    :initial
+  end
+
+  def execute(...)
+    DataPrivacyTask.create_with(
+      created_by_id: 1,
+      updated_by_id: 1
+    ).find_or_created_by(
+      deletable: record
+    )
+  end
+end
