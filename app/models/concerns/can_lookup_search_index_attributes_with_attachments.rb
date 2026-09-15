2ccac2313bfd5e627374dc97f0a68cb70f979d2f
Alisson Henrique Morais Braz <alissonhmb@gmail.com>
2026-09-15 14:34:45 -0300

    cloned can_lookup_search_index_attributes_with_attachments

---
+++ app/models/concerns/can_lookup_search_index_attributes_with_attachments.rb
@@ -0,0 +1,26 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+module CanLookupSearchIndexAttributesWithAttachments
+  extend ActiveSupport::Concern
+
+  def search_index_attachments_lookup(current_size)
+    attachments.filter_map do |attachment|
+      next unless search_index_attachment_indexable?(attachment, current_size)
+
+      attachment_attributes = SearchIndexBackend.attachment_to_attributes(attachment, current_size)
+      current_size += attachment_attributes['_size']
+
+      attachment_attributes
+    end
+  end
+
+  private
+
+  def search_index_attachment_indexable?(attachment, current_size)
+    return false if SearchIndexBackend.attachment_ignored?(attachment)
+    return false if SearchIndexBackend.attachment_too_big?(attachment)
+    return false if SearchIndexBackend.payload_too_big?(current_size + attachment.content.bigsize)
+
+    true
+  end
+end
