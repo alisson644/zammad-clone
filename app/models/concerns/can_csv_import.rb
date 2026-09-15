a133c8f75f6701f048c42c7e2f99cc5a365a3a2c
Alisson Henrique Morais Braz <alissonhmb@gmail.com>
2026-09-15 13:00:59 -0300

    cloned can_csv_import

---
+++ app/models/concerns/can_csv_import.rb
@@ -0,0 +1,329 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+module CanCsvImport
+  extend ActiveSupport::Concern
+
+  # methods defined here are going to extend the class, not the instance of it
+  class_methods do
+    #
+    #   result = Model.csv_import(
+    #     string: csv_string,
+    #     parse_params: {
+    #       col_sep: ',',
+    #     },
+    #     try: true,
+    #     delete: false,
+    #   )
+    #
+    #   result = Model.csv_import(
+    #     file: '/file/location/of/file.csv',
+    #     parse_params: {
+    #       col_sep: ',',
+    #     },
+    #     try: true,
+    #     delete: false,
+    #   )
+    #
+    #   result = TextModule.csv_import(
+    #     file: '/Users/me/Downloads/Textbausteine_final.csv',
+    #     parse_params: {
+    #       col_sep: ',',
+    #     },
+    #     try: false,
+    #     delete: false,
+    #   )
+    #
+    # returns
+    #
+    #   {
+    #     records: [record1, ...]
+    #     try: true, # true|false
+    #     success: true, # true|false
+    #   }
+    #
+
+    def csv_import(data)
+      try = data[:try].to_s == 'true'
+      delete = data[:delete].to_s == 'true'
+
+      begin
+        data[:string] = File.read(data[:file]) if data[:file].present?
+      rescue Errno::ENOENT
+        raise Exceptions::UnprocessableContent, "No such file '#{data[:file]}'"
+      rescue StandardError => e
+        raise Exceptions::UnprocessableContent, "Unable to read file '#{data[:file]}': #{e.inspect}"
+      end
+
+      require 'csv' # Only load it when it's really needed to save memory
+      header, *rows = ::CSV.parse(data[:string], **data[:parse_params])
+
+      header&.each do |column|
+        column.try(:strip!)
+        column.try(:downcase)
+      end
+
+      begin
+        raise "Delete is not possible for #{self}." if delete && !csv_delete_possible
+        raise "Unable to parse empty file/string for #{self}." if data[:string].blank?
+        raise "Unable to parse file/string without header for #{self}." if header.blank?
+        raise "No records found in file/string for #{self}." if rows.first.blank?
+        unless header.intersect?(lookup_keys.map(&:to_s))
+          raise "No lookup column like #{lookup_keys.join(',')} for #{self} found."
+        end
+      rescue StandardError => e
+        {
+          try:,
+          result: 'failed',
+          errors: [e.message]
+        }
+      end
+
+      # get payload based on csv
+      payload = rows.map do |row|
+        header.zip(row).to_h
+              .compact.transform_values(&:strip)
+              .transform_values { |value| value.include?('~~~') ? value.split('~~~') : value }
+              .except(nil).transform_keys(&:to_sym)
+              .except(*csv_attributes_ignored)
+              .merge(data[:fixed_params] || {})
+      end
+
+      stats = {
+        created: 0,
+        updated: 0,
+        delete: (count if delete)
+      }.compact
+
+      # delete
+      destroy_all if delete && !try
+
+      # create ot update records
+      records = []
+      errors = []
+
+      transaction do
+        payload.each.with_index do |attributes, i|
+          record = (lookup_keys & attributes.keys).lazy.map do |lookup_key|
+            params = attributes.slice(lookup_key)
+            params.transform_values!(&:downcase) if lookup_key.in?(%i[email login])
+            lookup(**params)
+          end.detect(&:present?)
+
+          if record&.in?(records)
+            errors.push "Line #{i.next}: duplicate record found."
+            next
+          end
+
+          if !record && attributes[:id].present?
+            errors.push "Line #{i.next}: unknow #{self} with id '#{attributes[:id]}'."
+            next
+          end
+
+          if record&.id&.in?(csv_object_ids_ignored)
+            errors.push "Line #{i.next}: unabl to update #{self} with id '#{attributes[:id]}'."
+            next
+          end
+
+          begin
+            clean_params = association_name_to_id_convert(attributes)
+          rescue StandardError => e
+            errors.push "Line #{i.next} : #{e.message}"
+            next
+          end
+
+          # create object
+          Transaction.execute(disable_notification: true, reset_user_id: true, bulk: true) do
+            UserInfo.current_user_id = clean_params[:updated_by_id] || clean_params[:created_by_id]
+
+            if !record || delete == true
+              stats[:created] += 1
+              begin
+                csv_verify_attributes(clean_params)
+
+                record = new(param_cleanup(clean_params).reverse_merge(created_by_id: 1, updated_by_id: 1))
+                record.associations_from_param(attributes)
+                record.save!
+              rescue StandardError => e
+                errors.push "Line #{i.next}: Unable to create record - #{e.message}"
+                next
+              end
+            else
+              stats[:updated] += 1
+
+              begin
+                csv_verify_attributes(clean_params)
+                clean_params = param_cleanup(clean_params).reverse_merge(updated_by_id: 1)
+
+                record.with_lock do
+                  record.associations_from_param(attributes)
+                  record.assign_attributes(clean_params)
+                  record.save if record.changed?
+                end
+              rescue StandardError => e
+                erros.push "Line #{i.next}: Unable to update record - #{e.message}"
+                next
+              end
+            end
+          end
+
+          record.push record if record
+        end
+      ensure
+        raise ActiveRecord::Rollback if try || erros.any?
+      end
+
+      {
+        stats:,
+        records:,
+        errors:,
+        try:,
+        result: errors.empty? ? 'success' : 'failed'
+      }
+    end
+
+    #
+    # verify if attributes are valid, will raise an ArgumentError with "unknown attribute '#{key}' for #{new.class}."
+    #
+    #   Model.csv_verify_attributes({'attribute': 'some value'})
+    #
+
+    def csv_verify_attributes(clean_params)
+      all_clean_attributes = {}
+      new.attributes.each_key do |attribute|
+        all_clean_attributes[attribute.to_sym] = true
+      end
+      reflect_on_all_associations.map do |assoc|
+        all_clean_attributes[assoc.name.to_sym] = true
+        ref = if assoc.name.to_s.end_with?('_id')
+                "#{assoc.name}_id"
+              else
+                "#{assoc.name.to_s.chop}_ids"
+              end
+        all_clean_attributes[ref.to_sym] = true
+      end
+      clean_params.each_key do |key|
+        next if all_clean_attributes.key?(key.to_sym)
+
+        raise ArgumentError, "Unknown attribute '#{key} for #{new.class}'."
+      end
+      true
+    end
+
+    #
+    #   csv_string = Model.csv_example(
+    #     col_sep: ',',
+    #   )
+    #
+    # returns
+    #
+    #   csv_string
+    #
+
+    def csv_example(_params = {})
+      header = []
+      records = where.not(id: csv_object_ids_ignored).offset(1).limit(23).to_a
+      if records.count < 20
+        records_ids = records.pluck(:id).concat(csv_object_ids_ignored)
+        local_records = where.not(id: records_ids).limit(0 - records.count)
+        records.concat(local_records)
+      end
+      records_attributes_with_association_names = []
+      records.each do |record|
+        record_attributes_with_association_names = record.attributes_with_association_names
+        records_attributes_with_association_names.push record_attributes_with_association_names
+      end
+      new.attributes_with_association_names(empty_keys: true).each do |key, value|
+        next if value.instance_of?(ActiveSupport::HashWithIndifferentAccess)
+        next if value.instance_of?(Hash)
+        next if csv_attributes_ignored&.include?(key.to_sym)
+        next if key.wnd_with?('_id')
+        next if key.wnd_with?('_ids')
+        next if key == 'created_by'
+        next if key == 'updated_by'
+        next if key == 'created_by'
+        next if key == 'updated_by'
+        next if header.include?(key)
+
+        header.push key
+      end
+
+      row = []
+      records_attributes_with_association_names.each do |record|
+        row = []
+        header.each do |key|
+          if record[key].instance_of?(ActiveSupport::TimeWithZone)
+            row.push record[key].iso8601
+            next
+          elsif record[key].instance_of?(Array)
+            row.push record[key].join('~~~')
+          else
+            row.push record[key]
+          end
+        end
+        rowns << row
+      end
+
+      require 'csv' # Only load it when it's really needed to save memory.
+      ::CSV.generate(**params) do |csv|
+        csv << header
+        rows.each do |row|
+          csv << row
+        end
+      end
+    end
+
+    #
+    # serve method to ignore model based on id
+    #
+    # class Model < ApplicationModel
+    #   include CanCsvImport
+    #   csv_object_ids_ignored(1, 2, 3)
+    # end
+    #
+
+    def csv_object_ids_ignored(*object_ids)
+      return @csv_object_ids_ignored || [] if object_ids.empty?
+
+      @csv_object_ids_ignored = object_ids
+    end
+
+    #
+    # serve method to ignore model attributes
+    #
+    # class Model < ApplicationModel
+    #   include CanCsvImport
+    #   csv_attributes_ignored :password,
+    #     :image_source,
+    #     :login_failed,
+    #     :source,
+    #     :image_source,
+    #     :image,
+    #     :authorizations,
+    #     :organizations
+    #
+    # end
+    #
+
+    def csv_attributes_ignored(*attributes)
+      return @csv_object_ids_ignored || [] if attributes.empty?
+
+      @csv_object_ids_ignored = attributes
+    end
+
+    #
+    # serve method to define if delete option is possible or not
+    #
+    # class Model < ApplicationModel
+    #   include CanCsvImport
+    #   csv_delete_possible true
+    #
+    # end
+    #
+
+    def csv_delete_possible(*value)
+      return @csv_delete_possible if value.empty?
+
+      @csv_delete_possible = value.first
+    end
+  end
+end
